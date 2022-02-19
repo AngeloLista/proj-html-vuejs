d2bb847f580026ff4b62b5e0e24336a3b1d01144
AngeloLista <abstractplayer@gmail.com>
2022-02-19 19:09:46 +0100

    created FooterTop Component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,105 +1,23 @@
 <template>
   <footer>
     <!-- Footer Top -->
-    <section id="footer-top">
-      <div
-        class="container w-75 py-5 mt-5 d-flex flex-column align-items-center"
-      >
-        <div class="row">
-          <!-- left -->
-          <div class="col-4 p-3 d-flex align-items-center flex-column">
-            <a href="#">
-              <img
-                class="img-fluid my-3"
-                src="../assets/img/avadabarbers_footer_shop_image.png"
-                alt="Avada Footer Shop"
-              />
-            </a>
-            <ButtonLink text="shop our range now" />
-          </div>
-          <!-- center -->
-          <div class="col-4 p-3 d-flex align-items-center flex-column">
-            <a href="#">
-              <img
-                class="img-fluid my-3"
-                src="../assets/img/avadabarbers-logo-x1.png"
-                alt="Avada Footer Shop"
-              />
-            </a>
-            <!-- Contacts -->
-            <div class="contacts">
-              <span class="text-muted text-center d-block">Avada Barbers</span
-              ><span class="text-muted text-center d-block"
-                >123 New York Street</span
-              ><span class="text-muted text-center d-block">New York City</span
-              ><span class="text-muted text-center d-block"
-                >info@yourwebsite.com</span
-              ><span class="text-muted text-center d-block"
-                >+1 (555) 555-1212</span
-              >
-            </div>
-
-            <!-- Socials -->
-            <div class="socials">
-              <h5 class="py-5">FOLLOW US</h5>
-              <div class="social-icons">
-                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-                <a href="#"><i class="fa-brands fa-twitter"></i></a>
-                <a href="#"><i class="fa-brands fa-youtube"></i></a>
-                <a href="#"><i class="fa-brands fa-instagram"></i></a>
-              </div>
-            </div>
-          </div>
-          <!-- right -->
-          <div class="col-4 p-3 d-flex align-items-center flex-column">
-            <a href="#">
-              <img
-                class="img-fluid my-3"
-                src="../assets/img/avadabarbers_footer_appointment_image.png"
-                alt="Avada Footer Shop"
-              />
-            </a>
-            <ButtonLink text="book your appoinment" />
-          </div>
-        </div>
-      </div>
-    </section>
+    <FooterTop />
   </footer>
 </template>
 
 <script>
-import ButtonLink from "./ButtonLink.vue";
+import FooterTop from "./Footer/FooterTop.vue";
 export default {
   name: "Footer",
-  components: { ButtonLink },
+  components: { FooterTop },
 };
 </script>
 
 <style scoped lang="scss">
 @import "../assets/scss/_vars.scss";
 @import "../assets/scss/_utils.scss";
+
 footer {
   font-family: $lato;
-  #footer-top {
-    background-color: $cod-gray;
-    .socials {
-      h5 {
-        color: $driftwood;
-        font-size: 13px;
-        text-align: center;
-        letter-spacing: 3px;
-        font-weight: bold;
-      }
-      .social-icons {
-        text-align: center;
-        a {
-          color: white;
-          & * {
-            padding: 0 6px;
-          }
-        }
-      }
-    }
-  }
 }
 </style>
